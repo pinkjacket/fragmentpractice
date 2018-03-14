6d13a2e844709f07d9c7843939b75f86f5a96925
Student <example@example.com>
2018-03-14 15:07:15 -0700

    dialog fragment response now logs to console

--- app/src/main/java/pinkjacket/fragmentsapp/MoodDialogFragment.java
+++ app/src/main/java/pinkjacket/fragmentsapp/MoodDialogFragment.java
@@ -2,19 +2,44 @@ package pinkjacket.fragmentsapp;
 
 import android.app.DialogFragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-/**
- * Created by Guest on 3/14/18.
- */
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MoodDialogFragment extends DialogFragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        View rootView = inflater.inflate(R.layout.fragment_mood_dialog, container, false);
-        getDialog().setTitle("Simple Dialog");
+        final View rootView = inflater.inflate(R.layout.fragment_mood_dialog, container, false);
+
+        Button cancelButton = (Button) rootView.findViewById(R.id.cancelButton);
+        Button submitButton = (Button) rootView.findViewById(R.id.submitButton);
+
+        final RadioGroup surveyRadioGroup = (RadioGroup) rootView.findViewById(R.id.moodRadioGroup);
+
+        cancelButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                dismiss();
+            }
+        });
+
+        submitButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                int selectedId = surveyRadioGroup.getCheckedRadioButtonId();
+                final RadioButton selectedRadioButton = (RadioButton) rootView.findViewById(selectedId);
+                Log.d("testing", selectedRadioButton.getText().toString());
+                dismiss();
+            }
+        });
+
+//        getDialog().setTitle("Simple Dialog");
         return rootView;
     }
 }
